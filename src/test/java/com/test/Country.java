e729b0193b1b95afa23c34b24921b68f51d1c579
Jeevi-123 <hlfjeevi@gmail.com>
2023-07-26 07:01:17 +0530

    Country code added by Person1

--- src/test/java/com/test/Country.java
+++ src/test/java/com/test/Country.java
@@ -13,4 +13,16 @@ public class Country {
 	public void name3() {
 		System.out.println("Austalia");
 	}
+	public void code1() {
+		System.out.println("01");
+
+	}
+
+	public void code2() {
+		System.out.println("02");
+	}
+
+	public void code3() {
+		System.out.println("03");
+	}
 }
