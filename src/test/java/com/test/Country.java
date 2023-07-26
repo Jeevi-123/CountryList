7878f6ce8a5b26e3ee7916087e44f04458d5a878
Jeevi-123 <hlfjeevi@gmail.com>
2023-07-26 06:51:52 +0530

    Country List

---
+++ src/test/java/com/test/Country.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Country {
+	public void name1() {
+		System.out.println("India");
+
+	}
+
+	public void name2() {
+		System.out.println("Canada");
+	}
+
+	public void name3() {
+		System.out.println("Austalia");
+	}
+}
---
+++ src/test/java/com/test/CountryCode.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class CountryCode {
+	public void code1() {
+		System.out.println("01");
+
+	}
+
+	public void code2() {
+		System.out.println("02");
+	}
+
+	public void code3() {
+		System.out.println("03");
+	}
+
+}
